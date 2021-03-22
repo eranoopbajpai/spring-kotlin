a4166b1dfc96ce1006afc52b3077fab64bde2a24
eranoopbajpai <eranoopbajpai@gmail.com>
2021-03-22 17:58:55 +0530

    Project created for Bamboo Spring boot learning
    Project created for Bamboo Spring boot learning

---
+++ src/main/kotlin/com/spkot/controller/GreetController.kt
@@ -0,0 +1,13 @@
+package com.spkot.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetController {
+
+    @GetMapping("/greet")
+    fun greet(): String {
+        return "Hello, World"
+    }
+}
